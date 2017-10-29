750ea0f2d8b741dbbd2a1812ff307f4e6c2bc3db
jhta <jeisonhs93@gmail.com>
2017-10-29 13:53:11 -0500

    add auth settings

---
+++ pages/login.js
@@ -0,0 +1,9 @@
+
+export default () => (
+  <div>
+    <h1> Spotify app </h1>
+    <a href="/auth">login</a>
+  </div>
+)
+
+
---
+++ utils/withAuth.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Router from 'next/router';
+import { persitsSession, isLogged, getToken } from './authPersistense';
+
+
+export default function withAuth(BaseComponent) {
+  class AuthComponent extends Component {
+    static async getInitialProps({ req, res, query }) {
+      let token = null;
+      if (req.session) token = req.session.token;
+      else if (query) token = query.access_token;
+      return { token };
+    }
+
+    render() {
+      return <BaseComponent {...this.props} isLogged />
+    }
+  }
+
+  return AuthComponent;
+}
