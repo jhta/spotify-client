124b00f153c0aab2059a97cf186c9b247dfe4c69
jhta <jeisonhs93@gmail.com>
2017-10-30 04:07:37 -0500

    add styled components

--- client/components/ui/Error.js
+++ client/components/ui/Error.js
@@ -1,5 +1,14 @@
 import { pure } from 'recompose';
+import styled from 'styled-components';
 
-const ErrorBox = pure(({ message }) => <div>{ message }</div>);
+const Box = styled.div`
+  background: #ddd;
+  color: black;
+  background: #00467F;
+  background: -webkit-linear-gradient(to right, #A5CC82, #00467F);
+  background: linear-gradient(to right, #A5CC82, #00467F);
+`;
+
+const ErrorBox = pure(({ message }) => <Box>{ message }</Box>);
 
 export default ErrorBox;
--- pages/login.js
+++ pages/login.js
@@ -1,22 +1,49 @@
 import { compose, withState } from 'recompose';
 import Loading from '../client/components/ui/Loading';
+import styled from 'styled-components';
 
 const enhance = compose(
   withState('isLoading', 'onLoading', false)
 );
 
+const Layout = styled.div`
+  background: #00467F;
+  background: -webkit-radial-gradient(#1ED760, #004670);
+  background: radial-gradient(#1ED760, #004670);
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Link = styled.a`
+  padding: 1rem 2rem;
+  border: 4px solid white;
+  color: white;
+  text-decoration: none;
+  min-width: 10em;
+  text-align: center;
+  font-weight: bold;
+  transition: all 600ms ease-in-out;;
+
+  &:hover {
+    color: #007de1;
+    border-color: #007de1;
+    transform: scale(1.1);
+  }
+`
 const Login = enhance(({ isLoading, onLoading }) => (
-  <div>
-    <h1> Spotify app </h1>
+  <Layout>
+    <img src="https://www.shareicon.net/download/2016/06/21/607432_spotify.ico" />
     {
       isLoading ?
         <Loading />
         :
-      <a href="/auth" onClick={() => { onLoading(true) }}>login</a>
+      <Link href="/auth" onClick={() => { onLoading(true) }}>LOGIN NOW!</Link>
     }
-      </div>
+      </Layout>
 ));
 
 export default Login;
-
-
