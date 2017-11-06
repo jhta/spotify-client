b347b4896dbf1c0337ccf8322cb8487c6b54ea64
jhta <jeisonhs93@gmail.com>
2017-11-06 18:05:14 -0500

    refactor bar form

--- client/components/search/Bar/index.js
+++ client/components/search/Bar/index.js
@@ -1,48 +1,37 @@
 import React, { Component } from 'react';
 import Input from '../../ui/styled/Input';
 import Form from '../../ui/styled/Form';
+import { compose, withHandlers, withState } from 'recompose';
 
 const PLACEHOLDER = "search: Bohemian Rhapsody";
 
-export default class SearchBar extends Component {
-
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      value: null
-    };
-  }
-
-  handleChange(event) {
-    this.setState({
-      value: event.target.value
-    })
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    const { value } = this.state;
-    this.setState({ value: '' });
-    if (value && value.length > 0) {
-      this.props.onSubmit(value);
+const enhace = compose(
+  withState('value', 'updateValue', ''),
+  withHandlers({
+    handleSubmit: ({ onSubmit, value, updateValue }) => event => {
+      event.preventDefault();
+      event.stopPropagation();
+      updateValue('');
+      if (value.length > 3) onSubmit(value);
+    },
+    handleChange: ({ updateValue  }) => event => {
+      updateValue(event.target.value)
     }
-  }
+  })
+);
 
-  render() {
-    return (
-      <div>
-      <Form onSubmit={this.handleSubmit}>
-        <Input
-          mt={this.props.mt}
-          type="text"
-          ref={(input) => { this.input = input }}
-          placeholder={PLACEHOLDER}
-          onChange={this.handleChange}/>
-      </Form>
-    </div>
-    )
-  }
-}
+const Bar = ({ mt, handleSubmit, handleChange }) => (
+  <div>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        mt={mt}
+        type="text"
+        placeholder={PLACEHOLDER}
+        onChange={handleChange}/>
+    </Form>
+  </div>
+);
+
+
+export default enhace(Bar);
+
